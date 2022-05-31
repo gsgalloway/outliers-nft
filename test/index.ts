8a7e7c763ab4a8b83a6d4bf1dab2d6440648c057
Gavi Galloway <gsgalloway@users.noreply.github.com>
2022-05-31 16:20:08 -0700

    add minting tests

--- test/index.ts
+++ test/index.ts
@@ -1,19 +1,63 @@
 import { expect } from "chai";
+import { Signer } from "ethers";
 import { ethers } from "hardhat";
+import { OutliersNFT } from "../typechain";
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+describe("OutliersNFT", function () {
+  let outliersNFT: OutliersNFT;
+  let admin: Signer;
+  let minter: Signer;
+  let nonApprovedMinter: Signer;
+  let minterRole: string;
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+  const TOKEN_NAME = "OutliersNFT";
+  const TOKEN_SYMBOL = "OUTLIERS";
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+  beforeEach("deploy contract", async function () {
+    // get signers
+    [admin, minter, nonApprovedMinter] = await ethers.getSigners();
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+    // deploy contract
+    const OutliersNFT = await ethers.getContractFactory("OutliersNFT");
+    outliersNFT = await OutliersNFT.deploy(
+      await admin.getAddress(),
+      TOKEN_NAME,
+      TOKEN_SYMBOL
+    );
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+    minterRole = await outliersNFT.MINTER_ROLE();
+  });
+
+  it("authorized minters should be able to mint exactly once", async function () {
+    // Authorize mint
+    await outliersNFT
+      .connect(admin)
+      .grantRole(minterRole, await minter.getAddress());
+
+    // Mint once
+    await outliersNFT.connect(minter).mint();
+
+    // Check that minter owns the newly minted token
+    const owner = await outliersNFT.ownerOf(0);
+    expect(owner).to.equal(await minter.getAddress());
+
+    // Mint again
+    const mintTx = outliersNFT.connect(minter).mint();
+    await expect(mintTx).to.be.revertedWith(
+      "OutliersNFT: must have minter role to mint"
+    );
+  });
+
+  it("unauthorized minters should not be allowed to mint", async function () {
+    // Authorize mint
+    await outliersNFT
+      .connect(admin)
+      .grantRole(minterRole, await minter.getAddress());
+
+    // Mint from different address should fail
+    const mintTx = outliersNFT.connect(nonApprovedMinter).mint();
+    await expect(mintTx).to.be.revertedWith(
+      "OutliersNFT: must have minter role to mint"
+    );
   });
 });
